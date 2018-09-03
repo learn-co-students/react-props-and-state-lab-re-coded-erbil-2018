28ae479e82ccf595cfa85a7aa9677cb64db578d6
Ahmad Khatab Ahmad <ahmadzendane725@gmail.com>
2018-09-03 18:03:32 +0000

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -11,16 +11,20 @@ class Pet extends React.Component {
         <div className="content">
           <a className="header">Pet name (gender: ♂ or ♀)</a>
           <div className="meta">
-            <span className="date">Pet type</span>
+            <span className="date">Pet type {this.props.type}</span>
           </div>
           <div className="description">
-            <p>Age: </p>
-            <p>Weight: </p>
+            <p>Age: {this.props.age}</p>
+            <p>Weight: {this.props.weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui primary button">Adopt pet</button>
-          <button className="ui disabled button">Already adopted</button>
+          {this.props.isAdopted 
+          ?  <button className="ui disabled button">Already adopted</button>
+          :<button onClick={this.props.onAdoptPet()}
+          className="ui primary button">Adopt pet</button>
+            
+          }
         </div>
       </div>
     );
